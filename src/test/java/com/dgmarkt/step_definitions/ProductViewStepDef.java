dbb40c8d6f8f8c14fb263160d7481c04df1cbc9f
furkan taşdemir <118552824+tekelfurkan@users.noreply.github.com>
2023-07-22 21:09:29 +0300

    ProductView

--- src/test/java/com/dgmarkt/step_definitions/ProductViewStepDef.java
+++ src/test/java/com/dgmarkt/step_definitions/ProductViewStepDef.java
@@ -1,19 +1,20 @@
 package com.dgmarkt.step_definitions;
 import com.dgmarkt.pages.CategoriesBasePage;
 import com.dgmarkt.pages.HomePage;
-import com.dgmarkt.pages.categoryPages.HealthAndBeautyPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 
-public class ProductViewStepDef extends HealthAndBeautyPage {
+public class ProductViewStepDef  {
     HomePage homePage = new HomePage();
     CategoriesBasePage categoryPage = new CategoriesBasePage();
+
 
     @Given("The user hovers over the category and selects one of the four sub-categories {string}from the sub-categories.")
     public void theUserHoversOverTheCategoryAndSelectsOneOfTheFourSubCategoriesFromTheSubCategories(String categoryName) {
         homePage.navigateToCategory(categoryName);{
+
         }
     }
     @When("user click on product view icon{string}")
--- src/test/resources/Features/PriceSlider.feature
+++ src/test/resources/Features/PriceSlider.feature
@@ -12,10 +12,11 @@ Feature:PRICE slider function
   Scenario Outline: Check if the product slider is visible by selecting a category from the sub-categories.
     Given The user hovers over the category and selects one of the four sub-categories "<Category Name>"from the sub-categories.
     When sees the price slider
+    And user enters a  "<priceRange>" and "<priceRangeMax>"
     Then verify if the user can use the price slider
     Examples:
-      | Category Name   |
-      | Health & Beauty |
-      | Televisions     |
-      | Networking      |
-      | TV Accessories  |
+      | Category Name   | priceRange | priceRangeMax |
+      | Health & Beauty | 8.09399    | 77.5457       |
+      | Televisions     | 8.09399    | 77.5457       |
+      | TV Accessories  | 8.09399    | 77.5457       |
+      | Networking      | 8.09399    | 77.5457       |
